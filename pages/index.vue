477c131f556cc8c79dea04a79dfb467b8340d774
Iroha71 <y.koba.h1071@outlook.jp>
2019-06-18 09:42:26 +0900

    update: カラーコードを表示するように変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
   div
-    h3 写真をアップロードしてください
+    h3(v-if="isUpLoaded") 写真がアップロードされました!
+    h3(v-else) 写真をアップロードしてください
     img(:src="imagePath")
     input(type="file" id="inputFile" @change="uploadImage")
     button(type="button" @click="getImagePredict" :disabled="isUploading" v-if="imagePath") 生成
     div
       div(v-for="c in colors" :style="{'background': c.raw_hex}").ball
+        p(v-if="c.raw_hex!=='#ffffff'") {{c.raw_hex}}
+        p.white(v-else) {{c.raw_hex}}
 </template>
 
 <script>
@@ -21,6 +24,7 @@ export default {
     return {
       imagePath: '',
       isUploading: false,
+      isUpLoaded: false,
       isImgDownloading: false,
       colors: []
     }
@@ -43,6 +47,7 @@ export default {
             console.log(`image url is ${url}`)
           })
         })
+        this.isUpLoaded=true
       }
     },
     getImagePredict:function(){
@@ -67,5 +72,20 @@ img{
   width: 100px;
   border-radius: 50%;
   display: inline-block;
+  position: relative;
+  p{
+    text-align: center;
+    position: absolute;
+    width: 80px;
+    height: 20px;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+  }
+  .white{
+    color: white;
+  }
 }
 </style>
